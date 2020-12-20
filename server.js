a9f93ffe8397e6f13e9ab3023047f41306f71414
girina <andre.girin@etu.univ-grenoble-alpesfr>
2020-12-20 22:15:11 +0100

    add ejs

--- server.js
+++ server.js
@@ -3,6 +3,18 @@ const path = require('path');
 const app = express();
 var mysql = require('mysql');
 
+app.set('view engine', 'ejs')
+
+app.get('/test', (request, response) => {
+    con.query("Select * from Shows", function (err, result) {
+        if (err) throw err;
+        console.log(result);
+        response.render('test', {data: result})
+      });
+})
+
+app.listen(8080)
+
 var con = mysql.createConnection({
     host: "eu-cdbr-west-03.cleardb.net",
     user: "b2887a7b5699c2",
@@ -18,29 +30,12 @@ con.connect(function(err) {
       console.log(result);
     });
 });
-/*
-con.connect(function(err) {
-    if (err) throw err;
-    console.log("Connected!");
-    con.query("CREATE DATABASE mydb", function (err, result) {
-      if (err) throw err;
-      console.log("Database created");
-    });
-});*/
+
 
 /*
-con.connect(function(err) {
-    if (err) throw err;
-    console.log("Connected!");
-    var sql = "CREATE TABLE shows (city VARCHAR(255), date DATETIME)";
-    con.query(sql, function (err, result) {
-      if (err) throw err;
-      console.log("Table created");
-    });
-  });*/
-
 app.use(express.static(__dirname + '/dist/angular-on-heroku'));
 app.get('/*', function(req,res) {
 res.sendFile(path.join(__dirname+
 '/dist/angular-on-heroku/index.html'));});
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 8080);*/
+
